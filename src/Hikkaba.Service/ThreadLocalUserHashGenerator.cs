dd357ba05d87abe2a786451ec7c3cb72a9f09245
Ilya Burakov <mailparser@mail.ru>
2016-12-27 11:03:21 +0800

    convert ThreadLocalUserHashGenerator component to service

---
+++ src/Hikkaba.Service/ThreadLocalUserHashGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hikkaba.Service
+{
+    public interface IThreadLocalUserHashGenerator
+    {
+        string Generate(string threadId, string userIpAddress);
+    }
+
+    public class ThreadLocalUserHashGenerator
+    {
+        private readonly ICryptoService _cryptoService;
+
+        public ThreadLocalUserHashGenerator(ICryptoService cryptoService)
+        {
+            _cryptoService = cryptoService;
+        }
+
+        public string Generate(string threadId, string userIpAddress)
+        {
+            return _cryptoService.HashHmacHex(threadId, userIpAddress);
+        }
+    }
+}
