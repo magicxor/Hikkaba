d52d7034a21d7f45c65037c2958023507d6962d0
Ilia Burakov <burakov.ilia@gmail.com>
2020-10-26 23:12:45 +0800

    fix js

--- Hikkaba.Web/WwwRootSources/js/site.js
+++ Hikkaba.Web/WwwRootSources/js/site.js
@@ -1,7 +1,8 @@
+'use strict';
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-var navigationFn = {
+window.navigationFn = {
     goToSection: function (id) {
         $("html, body").animate({
             scrollTop: $("#" + id).offset().top
@@ -9,78 +10,44 @@ var navigationFn = {
     }
 };
 
-$(function () {
-    $("time.time-localizable").each(function () {
-        var el = $(this);
-        var dt = moment(el.attr("datetime"));
-        el.text(dt.format("YYYY-MM-DD ddd HH:mm:ss"));
-    });
-
-    // if js is enabled and post form exists, prevent href=... and insert >>post id to form
-    var inputId = "new-post-message-input";
-    if ($("#" + inputId).length) {
-        $(".post-id-link").attr("onclick", "return false;");
-        $(".post-id-link").click(function () {
-            var el = $(this);
-            writeLineToInput("new-post-message-input", ">>" + el.text(), "");
-            writeSelectionLineToInput("new-post-message-input");
-            navigationFn.goToSection("new-post-message-input");
-        });
-    }
-
-    $(".datetimepicker-enabled").each(function () {
-        const dtDateOnly = 'YYYY-MM-DD';
-        const dtFull = dtDateOnly + ' HH:mm';
-        var el = $(this);
-        el.datetimepicker({
-            format: dtFull,
-            minDate: moment().format(dtDateOnly),
-            defaultDate: moment().format(dtDateOnly)
-        });
-    });
-});
-
-function getSelectionText() {
-    var text = "";
+window.getSelectionText = function getSelectionText() {
+    let text = "";
     if (window.getSelection) {
         text = window.getSelection().toString();
     } else if (document.selection && document.selection.type !== "Control") {
         text = document.selection.createRange().text;
     }
     return text;
-}
+};
 
-function writeSelectionLineToInput(inputId) {
-    var textArea = document.getElementById(inputId);
-
+window.writeSelectionLineToInput = function writeSelectionLineToInput(inputId) {
+    const textArea = document.getElementById(inputId);
     if (typeof (textArea) !== "undefined") {
-        var selectionText = getSelectionText();
+        const selectionText = getSelectionText();
         if (selectionText) {
             textArea.value += "[quote]" + getSelectionText() + "[/quote]\n";
         }
     }
-}
+};
 
-function writeLineToInput(inputId, text) {
-    var textArea = document.getElementById(inputId);
-
+window.writeLineToInput = function writeLineToInput(inputId, text) {
+    const textArea = document.getElementById(inputId);
     if (typeof (textArea) !== "undefined") {
         textArea.value += text + "\n";
     }
-}
+};
 
-function wrapText(inputId, openTag, closeTag) {
-    var textArea = document.getElementById(inputId);
-
+window.wrapText = function wrapText(inputId, openTag, closeTag) {
+    const textArea = document.getElementById(inputId);
     if (typeof (textArea.selectionStart) !== "undefined") {
-        var begin = textArea.value.substr(0, textArea.selectionStart);
-        var selection = textArea.value.substr(textArea.selectionStart, textArea.selectionEnd - textArea.selectionStart);
-        var end = textArea.value.substr(textArea.selectionEnd);
+        const begin = textArea.value.substr(0, textArea.selectionStart);
+        const selection = textArea.value.substr(textArea.selectionStart, textArea.selectionEnd - textArea.selectionStart);
+        const end = textArea.value.substr(textArea.selectionEnd);
         textArea.value = begin + openTag + selection + closeTag + end;
     }
-}
+};
 
-function insertTag(inputId, markupButtonId) {
+window.insertTag = function insertTag(inputId, markupButtonId) {
     switch (markupButtonId) {
         case "m-bold":
             wrapText(inputId, "[b]", "[/b]");
@@ -110,5 +77,37 @@ function insertTag(inputId, markupButtonId) {
             wrapText(inputId, "[sup]", "[/sup]");
             break;
         default:
+            break;
     }
-}
+};
+
+$(function () {
+    $("time.time-localizable").each(function () {
+        const thisElement = $(this);
+        const dateTimeControl = moment(thisElement.attr("datetime"));
+        thisElement.text(dateTimeControl.format("YYYY-MM-DD ddd HH:mm:ss"));
+    });
+
+    // if js is enabled and post form exists, prevent href=... and insert >>post id to form
+    const inputId = "new-post-message-input";
+    if ($("#" + inputId).length) {
+        $(".post-id-link").attr("onclick", "return false;");
+        $(".post-id-link").click(function () {
+            const thisElement = $(this);
+            writeLineToInput("new-post-message-input", ">>" + thisElement.text(), "");
+            writeSelectionLineToInput("new-post-message-input");
+            navigationFn.goToSection("new-post-message-input");
+        });
+    }
+
+    $(".datetimepicker-enabled").each(function () {
+        const dateFormatDateOnly = 'YYYY-MM-DD';
+        const dateFormatFull = dateFormatDateOnly + ' HH:mm';
+        const thisElement = $(this);
+        thisElement.datetimepicker({
+            format: dateFormatFull,
+            minDate: moment().format(dateFormatDateOnly),
+            defaultDate: moment().format(dateFormatDateOnly)
+        });
+    });
+});
