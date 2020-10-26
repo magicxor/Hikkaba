41ed3ecc94ac0b0e47fe40d5dbe172f19b88acee
Ilia Burakov <burakov.ilia@gmail.com>
2020-10-26 23:20:26 +0800

    fix js

--- Hikkaba.Web/WwwRootSources/js/site.js
+++ Hikkaba.Web/WwwRootSources/js/site.js
@@ -1,4 +1,3 @@
-'use strict';
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
