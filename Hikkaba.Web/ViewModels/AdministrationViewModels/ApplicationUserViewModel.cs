51532d2331d050b961adbed2497cbe3dd35425a9
Ilia Burakov <burakov.ilia@gmail.com>
2025-04-03 02:05:27 -0400

    Improves test coverage and development setup
    Enhances test workflows by adding coverage reporting and badge generation.
    
    Updates the Docker Compose configuration to use a newer SQL Server image and configures ports for development.
    
    Also upgrades package versions and adjusts the Dockerfile to include necessary dependencies.

--- Hikkaba.Web/ViewModels/AdministrationViewModels/ApplicationUserViewModel.cs
+++ Hikkaba.Web/ViewModels/AdministrationViewModels/ApplicationUserViewModel.cs
@@ -5,7 +5,7 @@ namespace Hikkaba.Web.ViewModels.AdministrationViewModels;
 public class ApplicationUserViewModel
 {
     public required int Id { get; set; }
-    public required string UserName { get; set; }
-    public required string Email { get; set; }
+    public required string? UserName { get; set; }
+    public required string? Email { get; set; }
     public required DateTime? LastLoginAt { get; set; }
 }
