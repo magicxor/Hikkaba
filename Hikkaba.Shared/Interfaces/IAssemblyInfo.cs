59cdf405af252117c479a4b6179d691c39bc8e51
Ilia Burakov <burakov.ilia@gmail.com>
2025-04-05 19:44:24 -0400

    add comment

--- Hikkaba.Shared/Interfaces/IAssemblyInfo.cs
+++ Hikkaba.Shared/Interfaces/IAssemblyInfo.cs
@@ -2,5 +2,9 @@
 
 public interface IAssemblyInfo
 {
+    /// <summary>
+    /// Assembly hierarchy level in this solution.
+    /// From lowest (0 = shared code and libraries) to highest (presentation layer).
+    /// </summary>
     public int HierarchyLevel { get; }
 }
