d50f4605edc897efa6c3a956ec5fabc84b379481
Ilia Burakov <burakov.ilia@gmail.com>
2025-04-07 00:08:14 -0400

    remove unused code

--- Hikkaba.Infrastructure.Models/Configuration/AuthCertificateCredsModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hikkaba.Infrastructure.Models.Configuration;
-
-public class AuthCertificateCredsModel
-{
-    public required string AuthCertificateBase64 { get; set; }
-
-    public required string AuthCertificatePassword { get; set; }
-}
