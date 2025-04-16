8331a71bafa7aa7fee6cc2845ddb1bf99c716584
Ilia Burakov <burakov.ilia@gmail.com>
2025-04-16 00:58:29 -0400

    remove comment

--- Hikkaba.Tests.Manual.LibManOptimizer/LibraryInfo.cs
+++ Hikkaba.Tests.Manual.LibManOptimizer/LibraryInfo.cs
@@ -4,6 +4,5 @@ internal sealed class LibraryInfo
 {
     public string? Library { get; set; }
     public string? Destination { get; set; }
-    // Use List<string> so we can modify it
     public IReadOnlyList<string>? Files { get; set; }
 }
