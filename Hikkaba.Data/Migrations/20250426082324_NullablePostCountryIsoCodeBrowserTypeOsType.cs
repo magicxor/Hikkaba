d6e3ad7ec55de4e860d47c28c9ffcaa138bb26f8
Ilia Burakov <burakov.ilia@gmail.com>
2025-04-26 04:25:56 -0400

    fix column nullability

---
+++ Hikkaba.Data/Migrations/20250426082324_NullablePostCountryIsoCodeBrowserTypeOsType.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hikkaba.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullablePostCountryIsoCodeBrowserTypeOsType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OsType",
+                table: "Posts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryIsoCode",
+                table: "Posts",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrowserType",
+                table: "Posts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OsType",
+                table: "Posts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryIsoCode",
+                table: "Posts",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrowserType",
+                table: "Posts",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
