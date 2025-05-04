dd9016cbe8257ad821c883287c511994e08c0f2b
Ilia Burakov <burakov.ilia@gmail.com>
2025-05-04 06:04:23 -0400

    add ValidationRegularExpressions.UsernameField

--- Hikkaba.Shared/Constants/ValidationRegularExpressions.cs
+++ Hikkaba.Shared/Constants/ValidationRegularExpressions.cs
@@ -1,9 +1,15 @@
-﻿namespace Hikkaba.Shared.Constants;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Hikkaba.Shared.Constants;
 
 public static class ValidationRegularExpressions
 {
-    public const string LatinCharsDigitsUnderscore = "^[a-zA-Z0-9_]*$";
-    public const string LatinCharsDigits = "^[a-zA-Z0-9]*$";
+    [StringSyntax(StringSyntaxAttribute.Regex)]
     public const string LowercaseLatinChars = "^[a-z]*$";
+
+    [StringSyntax(StringSyntaxAttribute.Regex)]
     public const string LowercaseLatinCharsDigitsUnderscore = "^[a-z0-9_]*$";
+
+    [StringSyntax(StringSyntaxAttribute.Regex)]
+    public const string UsernameField = @"(^[\p{L}\p{N}\p{P}]{0,100}##[\p{L}\p{N}\p{P}]{8,100}$)|(^(?!.*[#!])[\p{L}\p{N}\p{P}]*$)";
 }
